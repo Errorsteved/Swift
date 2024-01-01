35aa626655c6914ccf5ef9f008d860568c95d2fe
Errorsteved <liguanchenohg@163.com>
2024-01-01 15:26:33 +0800

    Initial Commit

---
+++ learning1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learning1
+//
+//  Created by errorsteve on 2024/1/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learning1/learning1App.swift
@@ -0,0 +1,17 @@
+//
+//  learning1App.swift
+//  learning1
+//
+//  Created by errorsteve on 2024/1/1.
+//
+
+import SwiftUI
+
+@main
+struct learning1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
