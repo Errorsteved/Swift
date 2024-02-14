439292c09db3758ab68535b6c2145d6777e92587
Errorsteved <liguanchenohg@163.com>
2024-02-14 10:48:58 +0800

    add isFaceUp

--- learning1/ContentView.swift
+++ learning1/ContentView.swift
@@ -9,12 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        HStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                Text("Hello, world!").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                CardView()
+            }
+            CardView(isFaceUp: false)
+            CardView()}
+    }
+}
+
+struct CardView:View {
+    var isFaceUp: Bool = true
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp{
+                Text("你好").font(.system(size: 50)).foregroundColor(.green)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                //.foregroundColor(.white)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+        .foregroundColor(.orange)
         .padding()
     }
 }
